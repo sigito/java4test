be3d471ffa4bdf0bf6944c57cd4139694a3a93a3
Yuri Samsoniuk <yuri.samsoniuk@gmail.com>
2013-04-01 07:21:44 +0300

    Exception thrown by field while construction makes constructor to throw it further.

---
+++ src/constructor/ExceptionInFieldConstructing.java
@@ -0,0 +1,37 @@
+package constructor;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+/**
+ * @author sigito
+ */
+public class ExceptionInFieldConstructing {
+    private static class FieldExceptionClass {
+        /**
+         * This field throws an exception while instantiating.
+         */
+        private InputStream iThrowExceptionWhileInstantiating = new FileInputStream("random unexist file name");
+
+        /**
+         * Constructor with explicit throws statement for the field {@code iThrowExceptionWhileInstantiating}.
+         *
+         * @throws FileNotFoundException from field initialized not in constructor.
+         */
+        private FieldExceptionClass() throws FileNotFoundException {
+        }
+    }
+
+    /**
+     * If we instantiate instance field with it's declaration,
+     * then if while instantiating this field exception might be thrown we should explicitly catch it in constructor.
+     */
+    public static void test() {
+        try {
+            new FieldExceptionClass();
+        } catch (FileNotFoundException e) {
+            System.out.println("Exception caught as supposed: " + e.getMessage());
+        }
+    }
+}
